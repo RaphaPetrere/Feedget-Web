8a9d2725b8cea30057c82b82ce6cca8cc30ecc49
Raphael Cardoso Petrere <raphaelpetrere@gmail.com>
2022-05-02 09:51:04 -0300

    ➕ Adding tailwindcss, postcss, phosphor-react for css and icons

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257e6',
+          200: '#a47dff'
+        },
+        fromBrand: {
+          500: '#8257e6',
+          200: '#a47dff'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
