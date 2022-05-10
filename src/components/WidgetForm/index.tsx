ca3dfe69f3f6a37d995f0f72e89d3687d564b704
Raphael Cardoso Petrere <raphaelpetrere@gmail.com>
2022-05-10 12:55:50 -0300

    ✨ Navigating from TypeStep to ContentStep and vice versa

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { CloseButton } from "../CloseButton"
 import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -33,19 +33,18 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export const WidgetForm = () => {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  const handleRestartFeedback = () => setFeedbackType(null);
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
       {!feedbackType 
         ?
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
         :
-        <p>
-          Hello World
-        </p>
+        <FeedbackContentStep 
+          feedbackType={feedbackType} 
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
       }
       <footer className="text-xs text-neutral-400">
         Feito com 💜 por <a className="underline underline-offset-2" href="https://github.com/RaphaPetrere">RaphaPetrere</a>
