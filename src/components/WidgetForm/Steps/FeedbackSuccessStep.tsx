ade03419dcc21d335fca6527680feadc050a09b4
RaphaPetrere <raphaelpetrere@gmail.com>
2022-05-10 15:17:13 -0300

    🍱 Adding success asset, finishing FeedbackSuccess step and navigating through Steps

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,3 +1,28 @@
-export const FeedbackSuccessStep = () => {
-  
+import { CloseButton } from "../../CloseButton"
+import successImageUrl from '../../../assets/success.svg';
+
+interface FeedbackSuccessStepProps {
+  onFeedbackRestartRequest: () => void;
+}
+
+export const FeedbackSuccessStep = ({onFeedbackRestartRequest}: FeedbackSuccessStepProps) => {
+  return (
+    <>
+      <header>
+        <CloseButton />
+      </header>
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img src={successImageUrl} alt="Ícone de sucesso" />
+        <span className="text-xl mt-2">Agradecemos o feedback!</span>
+
+        <button
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-[4px] border-transparent text-sm leading-6 hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          type="button"
+          onClick={onFeedbackRestartRequest}
+        >
+          Quero enviar outro
+        </button>
+      </div>
+    </>
+  )
 }
